d09d50ddfb26c5c8eb092072411b670cbd212e1a
Mathieu CHEVALIER <m.chevalier@groupeonepoint.com>
2023-08-03 15:58:53 +0200

    Added custom lambda support

--- main.tf
+++ main.tf
@@ -8,16 +8,27 @@ terraform {
 }
 
 data "http" "project_info" {
-  url = "https://catfact.ninja/fact"
-
+  url = "" # PUT LAMBDA URL HERE
+  request_body = jsonencode({ projectId = var.project_id})
   # Optional request headers
   request_headers = {
     Accept = "application/json"
   }
+
+  lifecycle {
+    postcondition {
+      condition     = jsondecode(self.response_body).Status != "unknown-project-id"
+      error_message = "Project id ${var.project_id} is not a valid project id."
+    }
+  }
 }
 
 locals {
-  remote_tags = jsondecode(data.http.project_info.response_body)
+  remote_tags = {
+    for key, value in jsondecode(data.http.project_info.response_body) :
+    key => key == "Status" ? null : value
+  }
+  # We remove the status value because it's not a tag
   default_tags = {
     Environment = var.is_production ? "True" : "False"
     Project     = var.project_id
