3558b99f2d6888c4ed435fc6d60f0494d25e530b
Mathieu CHEVALIER <m.chevalier@groupeonepoint.com>
2023-08-10 15:44:28 +0200

    Documentation

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ module "tags" {
   source = "github.com/m-chevalier/terraform-module-tagging"
 
   is_production = true
-  project_id    = "my-project"
+  project_id    = "my-project-id"
 }
 ```
 
@@ -22,42 +22,25 @@ provider "aws" {
 }
 ```
 
-## Ajouter des tags spécifiques sur une ressource
+## Mettre les tags uniquement sur une ressource
 
 ```tf
 resource "aws_s3_bucket" "example" {
   bucket = "test-bucket-tags"
-  
-  tags = {
-    Test = "valeur"
-  }
-}
-```
-
-Cette ressource aura pour tags à la fois les tags communs ainsi que le tag `Test : valeur`
-
-## Mettre les tags du module sur une ressource
-
-Dans le cas où `default_tags` n'a pas été spécifié, il est possible d'ajouter les `common_tags` à une ressource :
-
-```tf
-resource "aws_s3_bucket" "example" {
-  bucket = "test-bucket-tags"
-  
   tags = module.tags.common_tags
 }
 ```
 
-Il est aussi possible d'ajouter des tags spécifiques à cette ressource :
+## Ajouter des tags supplémentaires au module
 
 ```tf
-resource "aws_s3_bucket" "example" {
-  bucket = "test-bucket-tags"
-  
-  tags = merge(module.tags.common_tags,{
-    Test = "Valeur"
-  })
+module "tags" {
+  source = "github.com/m-chevalier/terraform-module-tagging"
+
+  is_production = true
+  project_id    = "my-project-id"
+  additional_tags = {
+    Nom         = "valeur"
+  }
 }
 ```
-
-Cette ressource aura ainsi les tags communs ainsi que le tag spécifique `Test : Valeur`
--- main.tf
+++ main.tf
@@ -13,10 +13,12 @@ provider "aws" {
 data "aws_lambda_invocation" "lambda_projectinfos" {
   function_name = var.project_info_lambda_name
 
+  # We send either the projectID parameter or the default projectID set on the AWS account
   input = jsonencode({ projectId = local.valid_project_id ? var.project_id : data.aws_organizations_resource_tags.account[0].tags.ProjectID})
 
   lifecycle {
     postcondition {
+      # We check if the lambda status is equals 'success' otherwise it will produce an error
       condition     = jsondecode(self.result).Status == "success"
       error_message = "Project id ${var.project_id} is not a valid project id."
     }
@@ -32,17 +34,21 @@ data "aws_organizations_resource_tags" "account" {
 
 locals {
   valid_project_id = var.project_id != ""
+
+  # We remove the status value returned by the lambda because it's not a tag
   remote_tags = {
     for key, value in jsondecode(data.aws_lambda_invocation.lambda_projectinfos.result) :
     key => key == "Status" ? null : value
   }
-  # We remove the status value because it's not a tag
+  
+  # We set the default tags (that are set localy)
   default_tags = {
     Environment = var.is_production ? "True" : "False"
     ProjectID   = local.valid_project_id ? var.project_id : data.aws_organizations_resource_tags.account[0].tags.ProjectID
     IaC         = "Terraform"
     Requester = data.aws_caller_identity.current.arn
   }
-  #Default tags are locally defined tags 
+
+  #We merge the default tags with the tags we obtained from the lambda 
   common_tags = merge(var.additional_tags, local.default_tags, local.remote_tags)
 }
